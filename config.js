5ef9aab045bcd7433b1779fd9b9fc5486e581a94
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2021-04-29 04:36:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    "ownerID": "519236223899009037", //kendi IDınızı yazınız
+    "admins": ["519236223899009037"],
+    "support": ["519236223899009037"],
+    "token": "NzIwMzExMjg5ODk2ODk0NjA1", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "", //botunuzun secretini yazınız
+      "callbackURL": `https://syat9.sayware.net/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "super-secret-session-thing", //kalsın
+      "domain": "https://syat9.sayware.net/", //site URLnizi yazınız!
+          "port": "443"
+    }
+  };
+  
+  module.exports = config;
+
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+
+//GİZLYİE ALMASANIZ SUNUCUNUZ PATLIYABİLİR
